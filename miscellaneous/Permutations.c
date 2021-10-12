a727d768f9c7bb787eee8fc5ab4040ed5a02edb2
AshrafNadir <nadir.tafadar@gmail.com>
2021-10-12 12:20:08 +0600

    ...

---
+++ miscellaneous/Permutations.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+
+void permute(char *s,int l,int r)
+{
+    int i;
+    if(l==r)
+        printf("%s\n",s);
+    else
+    {
+        for(i=l;i<=r;i++)
+        {
+            swap((s+l),(s+i));
+            permute(s,l+1,r);
+            swap((s+l),(s+i));
+        }
+    }
+}
+
+void swap(char *a, char *b)
+{
+    char temp;
+    temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+
+int main()
+{
+    char str[]="ABC";
+    int n=strlen(str);
+    permute(str,0,n-1);
+
+    return 0;
+}
